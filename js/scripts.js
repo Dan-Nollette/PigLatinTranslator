bf8e8c9f97015b8d7e367a12b8aa79519142d479
Dan Nollette and Linda Luu <linda@>
2017-08-16 10:22:17 -0700

    repurpose leapyear code for pigLatin and add initial behavior to do nothing to non-letter characters.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,12 @@
 $(document).ready(function() {
-  $("form#leap-year").submit(function(event){
+  $("form#pig-latin-input").submit(function(event){
     event.preventDefault();
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-    $("p").show();
-    if (result) {
-      $("#result").text("");
-    } else {
-      $("#result").text("not ");
-    }
+    var phrase = $("input#phrase").val();
+    var result = translate(phrase);
+    $("#result").text(result);
   });
 });
 
-var leapYear = function(year) {
-  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)){
-    return true;
-  } else {
-    return false;
-  }
+var translate = function(phrase) {
+  return phrase;
 };
