f8744ea41fc51b6fca6c5d69afbc183b585dce29
Dan Nollette and Linda Luu <linda@>
2017-08-16 10:43:09 -0700

    add behavior to append ay to the end of single letter words starting with consonants.

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,18 @@ $(document).ready(function() {
 });
 
 var translate = function(phrase) {
+  var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+  var isConsonant = false;
+  consonants.forEach(function(consonant) {
+    if (consonant === phrase) {
+      isConsonant = true;
+    }
+  });
+  if (isConsonant){
+    return phrase + "ay";
+  }
   if (phrase === "a" || phrase === "e" || phrase === "i" || phrase === "o" || phrase === "u") {
     return phrase + "way";
-  }
+  } if (phrase )
   return phrase;
 };
